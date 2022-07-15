6a7c0fed1be1e942904c56ab22132557dbe16d7b
Pierce Petit <ppetit2496@gmail.com>
2022-07-15 13:47:16 -0500

    adding create commit

---
+++ src/pages/AddEvent/AddEvent.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'
+//import styles from './AddEvent.module.css'
+
+const AddEvent = (props) => {
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    eventName: '',
+    eventDate: {type: Date},
+    eventDetails: '',
+
+  })
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      props.handleAddEvent(formData)
+      navigate('/')
+      // change to item and act page ^
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const {eventName, eventDate, eventDetails } = formData
+
+  const isFormInvalid = () => {
+    return !(eventName && eventDate)
+  }
+
+  return (
+    <>
+      <h1>Add Event</h1>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <div>
+          <label htmlFor="eventName">Event Name</label>
+          <input
+            type="text"
+            autoComplete="off"
+            id="eventName"
+            value={eventName}
+            name="eventName"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="eventDate">Event Date</label>
+          <input
+            type="date"
+          />
+        </div>
+        <div>
+          <label htmlFor="eventDetails">Event Details</label>
+          <textarea
+            autoComplete="off"
+            id="eventDetails"
+            value={eventDetails}
+            name="eventDetails"
+            onChange={handleChange}
+          />
+        </div>
+        <button disabled={isFormInvalid()}>
+          Add Event
+        </button>
+        <Link to="/">
+          <button>Cancel</button>
+        </Link>
+      </form>
+    </>
+  )
+}
+
+export default AddEvent;
---
+++ src/services/eventService.js
@@ -0,0 +1,49 @@
+import * as tokenService from '../services/tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/events`
+
+async function create(eventData) {
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: { 
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(eventData)
+  })
+  return await res.json()
+}
+
+async function getAll() {
+  const res = await fetch(BASE_URL)
+  return await res.json()
+}
+async function deleteEvent(eventId) {
+  const res = await fetch(`${BASE_URL}/${eventId}`, {
+    method: "DELETE",
+    headers: { 
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+    }
+  })
+  return await res.json()
+}
+
+async function updateEvent(eventData) {
+  const res = await fetch(`${BASE_URL}/${eventData._id}`, {
+    method: "PUT",
+    headers: { 
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(eventData)
+  })
+  return await res.json()
+}
+
+
+export { 
+  create,
+  getAll,
+  deleteEvent,
+  updateEvent
+}
