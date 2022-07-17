14cf10a94cc2e08970bcdbbdddc052c21254192c
Kathy White <kawhite8503@gmail.com>
2022-07-17 14:23:44 -0500

    add components on EventShow page

--- src/pages/EventShow/EventShow.jsx
+++ src/pages/EventShow/EventShow.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
+import GuestList from '../../components/GuestList/GuestList'
+import Activities from '../../components/Activities/Activities'
+import FoodSupplies from '../../components/FoodSupplies/FoodSupplies'
+import CommentForm from '../../components/CommentForm/CommentForm'
+import CommentForm from '../../components/CommentForm/CommentForm';
+
 
 const EventShow = (props) => {
-  const [component, setComponent] = useState(<GuestList/>)
+  const [component, setComponent] = useState('GuestList')
 
   if(!props.events.length){
     return <h1>No Events</h1>
@@ -14,13 +20,21 @@ const EventShow = (props) => {
     </div> 
     <div id='mainright-showPage'>
       <div id='mainright-left'>
-        <button onClick={() => setComponent(<GuestList />)}>Guest List</button>
-        <button onClick={() => setComponent(<Activities />)}>Activities</button>
-        <button onClick={() => setComponent(<FoodSupplies />)}>Food/Supplies</button>
-        <button onClick={() => setComponent(<Comments />)}>Comments</button>
+        <button onClick={() => setComponent('GuestList')}>Guest List</button>
+        <button onClick={() => setComponent('Activities')}>Activities</button>
+        <button onClick={() => setComponent('FoodSupplies')}>Food/Supplies</button>
+        <button onClick={() => setComponent('Comments')}>Comments</button>
       </div>
       <div id='mainright-right'>
-        {component}
+        if({component === 'GuestList' && <GuestList />}) {
+          <GuestList />
+        } else if({component === 'Activities' && <Activities />}) {
+          <Activities />
+        } else if({component === 'FoodSupplies' && <FoodSupplies />}) {
+          <FoodSupplies />
+        } else({component === 'Comments' && <CommentForm />}) {
+          <CommentForm />
+        }
       </div>
     </div>
     
