b9b3ada8c507e5c72a092af126d35c5c4aaef8ff
ilysim <illymarie1@live.com>
2022-07-18 12:08:51 -0500

    fix navbar and landing for user

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
     <header>
-      {user ?
+      {user &&
         <nav>
           Welcome, {user.name}
           <Link to="/">Events</Link>
@@ -12,11 +12,6 @@ const NavBar = ({ user, handleLogout }) => {
           <Link to='/all'>All Events</Link>
           <Link to="" onClick={handleLogout}>LOG OUT</Link>
           <Link to="/changePassword">Change Password</Link>
-        </nav>
-      :
-        <nav>
-          <Link to="/login">Log In</Link>
-          <Link to="/signup">Sign Up</Link>
         </nav>
       }
     </header>
