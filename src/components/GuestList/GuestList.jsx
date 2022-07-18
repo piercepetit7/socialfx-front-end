4d0163f145450107ea3bb4327bfac101638a6792
Pierce Petit <ppetit2496@gmail.com>
2022-07-18 18:17:04 -0500

    fix the show page

---
+++ src/components/GuestList/GuestList.jsx
@@ -0,0 +1,9 @@
+const GuestList = () => {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+}
+
+export default GuestList;
--- src/pages/EventShow/EventShow.jsx
+++ src/pages/EventShow/EventShow.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react';
-import GuestList from '../../components/GuestList/GuestList'
-import Activities from '../../components/Activities/Activities'
+import { useState, useEffect, React } from 'react';
+//import GuestList from '../../components/GuestList/GuestList'
+//import Activities from '../../components/Activities/Activities'
 import FoodSupplies from '../../components/FoodSupplies/FoodSupplies'
 import CommentForm from '../../components/CommentForm/CommentForm'
-
-
+import { show } from '../../services/eventService'
+import { useLocation } from 'react-router-dom';
 
 const EventShow = (props) => {
-  const [component, setComponent] = useState('GuestList')
+  const [component, setComponent] = useState('Comments')
+  const [event, setEvent] = useState({}) 
+  const location = useLocation()
+  console.log('*************************')
+  console.log(typeof location.state.event._id)
 
-  if(!props?.events?.length){
-    return <h1>No Events</h1>
-  }
+  useEffect(()=>{
+    const fetchEvent = async() => {
+      const eventData = await show(location.state.event._id)
+      console.log('EVENTDATA',eventData)
+      setEvent(eventData)
+    }
+    fetchEvent()
+  },[])
+
+  // if(!props?.events?.length){
+  //   return <h1>No Events</h1>
+  //   console.log("here",props.events)
+  // }
 
   return (
     <>
@@ -20,16 +34,16 @@ const EventShow = (props) => {
     </div> 
     <div id='mainright-showPage'>
       <div id='mainright-left'>
-        <button onClick={() => setComponent('GuestList')}>Guest List</button>
-        <button onClick={() => setComponent('Activities')}>Activities</button>
+        {/* <button onClick={() => setComponent('GuestList')}>Guest List</button> */}
+        {/* <button onClick={() => setComponent('Activities')}>Activities</button> */}
         <button onClick={() => setComponent('FoodSupplies')}>Food/Supplies</button>
         <button onClick={() => setComponent('Comments')}>Comments</button>
       </div>
       <div id='mainright-right'>
-        { component === 'GuestList' && <GuestList /> }
-        { component === 'Activities' && <Activities /> }
-        { component === 'FoodSupplies' && <FoodSupplies /> }
-        { component === 'Comments' && <CommentForm /> }
+        {/* { component === 'GuestList' ? <GuestList />: "" } */}
+        {/* { component === 'Activities' ? <Activities />: "" } */}
+        { component === 'FoodSupplies' ? <FoodSupplies />: "" }
+        { component === 'Comments' ? <CommentForm />: "" }
       </div>
     </div>
     
