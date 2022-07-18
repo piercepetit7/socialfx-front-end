4bff7648485a423590482ad0610386c54ac227b3
Kathy White <kawhite8503@gmail.com>
2022-07-18 13:39:27 -0500

    commit app.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Landing user={user}/>} />
             <Route path="/add" element={<AddEvent handleAddEvent={handleAddEvent} />} />
-            <Route path="/all" element={<EventList events={events} setEvents={setEvents}/>} />
+            <Route path="/all" element={<EventList events={events} user={user} setEvents={setEvents}/>} />
             <Route path="/events/:eventId/details" element={<AddDetails handleAddEvent={handleAddEvent} />} />
             <Route
               path="/signup"
