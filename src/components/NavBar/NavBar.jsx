4cc6f6e39d08054dea0d93c21f9944138018252b
ilysim <illymarie1@live.com>
2022-07-18 16:48:53 -0500

    implement navbar changes & landing paragraph

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
     <header>
       {user &&
-        <nav>
-          Welcome, {user.name}
-          <Link to="/">Events</Link>
-          <Link to="/add"> Add Events</Link>
-          <Link to='/all'>All Events</Link>
-          <Link to="" onClick={handleLogout}>LOG OUT</Link>
-          <Link to="/changePassword">Change Password</Link>
-        </nav>
+        <nav className={styles.nav}>
+        <p className={styles.username}>Welcome, {user.name}</p>
+        <Link to="/" className={styles.event}>Home</Link>
+        <Link to="/add" className={styles.add_event}> Add Events</Link>
+        <Link to='/all' className={styles.all_events}>All Events</Link>
+        <Link to="" onClick={handleLogout} className={styles.log_out}>LOG OUT</Link>
+        <Link to="/changePassword" className={styles.change}>Change Password</Link>
+      </nav>
       }
     </header>
     </>
