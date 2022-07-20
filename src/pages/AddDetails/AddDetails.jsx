65d2a40046c51d7235a13fd4081af6e3d2930436
Pierce Petit <ppetit2496@gmail.com>
2022-07-20 16:48:34 -0500

    working details

--- src/pages/AddDetails/AddDetails.jsx
+++ src/pages/AddDetails/AddDetails.jsx
@@ -5,7 +5,7 @@ import ActForm from './components/AddAct';
 import AddGuestList from './components/AddGuestList';
 import ItemForm from './components/AddItem';
 import Profiles from '../Profiles/Profiles';
-
+import * as eventServices from "../../services/eventService"
 
 const AddDetails = (props) => {
   const {eventId} = useParams()
@@ -15,39 +15,31 @@ const AddDetails = (props) => {
   const [activities, setAct] = useState([])
   const [items, setItems] = useState([])
   const [guests, setGuests] =useState([])
-  // const [formData, setFormData] = useState({
-  //   activities: '',
-  //   guestList: '',
-  //   items: '',
-  //})
-  // const handleChange = e => {
-  //   setFormData({
-  //     ...formData,
-  //     [e.target.name]: e.target.value,
-  //   })
-  // }
-  // const handleSubmit = async e => {
-  //   e.preventDefault()
-  //   try {
-  //     //props.handleUpdateEvent(formData)
-  //     // navigate('/')
-  //     // change to item and act page ^
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const form = {
+        _id: eventId,
+        guestList:guests, 
+        activities: activities,
+        items: items, 
+      }
+      const res = await eventServices.updateEvent(form)
+      console.log(res);
+      navigate(`/events/${eventId}`,{state:{event}})
+    } catch (err) {
+      console.log(err)
+    }
+  }
   useEffect(()=>{
     const fetchEvent = async() => {
       const eventData = await show(eventId)
-      console.log('EVENTDATA',eventData)
       setEvent(eventData)
     }
     fetchEvent()
   },[])
   
-
-
-  //const {activities, guestList, items} = formData
 
   
   return (
@@ -89,12 +81,13 @@ const AddDetails = (props) => {
           <ul>
             {guests.map(guest => 
               <li key={guest._id}>
-                {guest._id}
+                {guest.guestList}
               </li>
             )
           }
           </ul>
         </div>
+        <button onClick={handleSubmit}>submit</button>
       </div>
     </>
   )
--- src/pages/AddDetails/components/AddGuestList.jsx
+++ src/pages/AddDetails/components/AddGuestList.jsx
@@ -3,13 +3,11 @@ import * as profileService  from "../../../services/profileService"
 
 const AddGuestList = (props) => {
   const formElement = useRef()
-  const [formData, setFormData] =useState({
-    guestList: ''
-  })
+  const [formData, setFormData] =useState('')
   const [profiles, setProfiles] = useState([])
-
+console.log(formData);
   const handleChange = evt => {
-    setFormData({...formData, [evt.target.name]: evt.target.value})
+    setFormData(evt.target.value)
   }
 
   useEffect(() => {
@@ -19,24 +17,21 @@ const AddGuestList = (props) => {
     }
     fetchProfiles()
   }, [])
-console.log('****Fetched People YO!****',profiles)
+//console.log('****Fetched People YO!****',profiles)
 
 const handleGuestSubmit = evt => {
   evt.preventDefault()
   props.setGuests([...props.guests, formData ])
-  console.log('****ADD Guest*****')
-  console.log('*formData****Guest****',formData)
+  setFormData('')
 }
-console.log("*****Guests****",props.guests)
-
   return (
     <> 
     <form ref={formElement} onSubmit={handleGuestSubmit}>
       <label>
         Who's Invited?
       </label>
-      <select name="guests" onChange={handleChange}>
-        <option>pick guest</option>
+      <select name="guestList" onChange={handleChange}>
+        <option value={null}>pick guest</option>
           {profiles.map(profile => 
             <option value={profile._id} key={profile._id}>
               {profile.name}
