c2cfc2edd51863ec86ddc1b6cdf5ded482c3e2a6
Kathy White <kawhite8503@gmail.com>
2022-07-20 17:34:05 -0500

    take out console.log

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import styles from './Activities.module.css'
+import ActForm from '../../pages/AddDetails/components/AddAct';
 
 const Activities = (props) => {
+  const [activities, setAct] = useState([])
+
   return (
     <>
       <div className={styles.main}>
         <div className={styles.mainNextDiv}>
           <div className={styles.commentArea}>
-            <h1>Activities Tab</h1>
-            <p>props.event.activities.actName</p>
+            {/* <h1>Activities Tab</h1>
+            <div>
+              <ul>
+              {props.activities.map(activity => 
+                <li key={activity._id}>
+                  {activity.actName}
+                </li>
+              )}
+              </ul>
+            </div>
+            <ActForm setAct={setAct} activities={activities}/> */}
           </div>
         </div>
       </div>
@@ -16,3 +29,4 @@ const Activities = (props) => {
 }
 
 export default Activities;
+
