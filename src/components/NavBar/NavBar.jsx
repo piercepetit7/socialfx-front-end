6728ea24c8077ec676c8a67b246f02ca49af3ead
Kathy White <kawhite8503@gmail.com>
2022-07-20 15:46:33 -0500

    styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
+  console.log('==========', user.profile.photo)
   return (
     <>
     <header>
