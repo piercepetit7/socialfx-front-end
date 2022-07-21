dbb9c589d15d20439290fc90dff77d1ee55cb226
ilysim <illymarie1@live.com>
2022-07-21 09:55:50 -0500

    fix carousel & button hover color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,7 +9,6 @@ const NavBar = ({ user, handleLogout }) => {
       {user &&
         <nav className={styles.nav}>
         <p className={styles.username}>Welcome, {user.name}</p>
-        <Link id={styles.nav} to="/" className={styles.event}>Home</Link>
         <Link id={styles.nav} to="/add" className={styles.add_event}> Add Events</Link>
         <Link id={styles.nav} to='/all' className={styles.all_events}>All Events</Link>
         <Link id={styles.nav} to="/changePassword" className={styles.change}>Change Password</Link>
