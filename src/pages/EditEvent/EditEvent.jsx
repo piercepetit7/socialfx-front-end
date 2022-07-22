f3cdd7a093fd2c40b0e7cedab80a40581ef5b8c1
ilysim <illymarie1@live.com>
2022-07-21 23:00:00 -0500

    minor fixes

--- src/pages/EditEvent/EditEvent.jsx
+++ src/pages/EditEvent/EditEvent.jsx
@@ -43,61 +43,63 @@ const EditEvent = (props) => {
 
   return (
     <>
-      <h1 className={styles.header}>Edit Event</h1>
-      <form onSubmit={handleSubmit} autoComplete="off">
-        <div className={styles.name}>
-          <label htmlFor="eventName" className={styles.name_des}>Event Name:</label>
-          <input
-            type="text"
-            autoComplete="off"
-            id="eventName"
-            value={eventName}
-            name="eventName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.date}>
-          <label htmlFor="eventDate" className={styles.date_des}>Event Date: </label>
-          <input
-            type="datetime-local"
-            id="eventDate"
-            value={DateTime.fromISO(eventDate).toLocal().toISO().slice(0,16)}
-            name="eventDate"
-            onChange={handleChange}
-            className={styles.date_input}
-          />
-        </div>
-        <div className={styles.details}>
-          <label htmlFor="eventDetails" className={styles.event_des}>Event Details: </label>
-          <textarea
-            autoComplete="off"
-            id="eventDetails"
-            value={eventDetails}
-            name="eventDetails"
-            onChange={handleChange}
-          />
-        </div>
-        {/* <div className={styles.photo}>
-          <label htmlFor="photo-upload" className={styles.photo_des}>
-            Upload Photo
-          </label>
-          <input
-            type="file"
-            id="photo-upload"
-            className="form-control"
-            name="photo"
-            onChange={handleChangePhoto}
-          />
-        </div> */}
-        <div className={styles.buttons}>
-        <button disabled={isFormInvalid()} type='submit' className={styles.edit}>
-          Edit Event
-        </button>
-        <Link to="/all">
-          <button className={styles.cancel}>Cancel</button>
-        </Link>
-        </div>
-      </form>
+      <div id={styles.maindiv}>
+        <h1 className={styles.header}>Edit Event</h1>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <div className={styles.name}>
+            <label htmlFor="eventName" className={styles.name_des}>Event Name:</label>
+            <input
+              type="text"
+              autoComplete="off"
+              id="eventName"
+              value={eventName}
+              name="eventName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.date}>
+            <label htmlFor="eventDate" className={styles.date_des}>Event Date: </label>
+            <input
+              type="datetime-local"
+              id="eventDate"
+              value={DateTime.fromISO(eventDate).toLocal().toISO().slice(0,16)}
+              name="eventDate"
+              onChange={handleChange}
+              className={styles.date_input}
+            />
+          </div>
+          <div className={styles.details}>
+            <label htmlFor="eventDetails" className={styles.event_des}>Event Details: </label>
+            <textarea
+              autoComplete="off"
+              id="eventDetails"
+              value={eventDetails}
+              name="eventDetails"
+              onChange={handleChange}
+            />
+          </div>
+          {/* <div className={styles.photo}>
+            <label htmlFor="photo-upload" className={styles.photo_des}>
+              Upload Photo
+            </label>
+            <input
+              type="file"
+              id="photo-upload"
+              className="form-control"
+              name="photo"
+              onChange={handleChangePhoto}
+            />
+          </div> */}
+          <div className={styles.buttons}>
+          <button disabled={isFormInvalid()} type='submit' className={styles.edit}>
+            Edit Event
+          </button>
+          <Link to="/all">
+            <button className={styles.cancel}>Cancel</button>
+          </Link>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
